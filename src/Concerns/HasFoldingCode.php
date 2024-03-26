8f4ba298433850cdc44ec68240e36f00de2017db
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2024-03-26 16:23:52 +0000

    WIP

--- src/Concerns/HasFoldingCode.php
+++ src/Concerns/HasFoldingCode.php
@@ -7,6 +7,7 @@ use Closure;
 trait HasFoldingCode
 {
     protected bool|Closure $hasFoldingCode = true;
+    protected bool|Closure $hasFoldedCode = false;
 
     public function foldingCode(bool|Closure $condition = true): static
     {
@@ -18,5 +19,18 @@ trait HasFoldingCode
     public function getHasFoldingCode(): bool
     {
         return (bool) $this->evaluate($this->hasFoldingCode);
+    }
+
+    public function foldedCode(bool|Closure $condition = true): static
+    {
+        $this->hasFoldingCode = $condition;
+        $this->hasFoldedCode = $condition;
+
+        return $this;
+    }
+
+    public function getHasFoldedCode(): bool
+    {
+        return (bool) $this->evaluate($this->hasFoldedCode);
     }
 }
